78319ae48b059288420b542ebc739ae6bb2a99f4
yulianagomez <ygomez297@gmail.com>
2017-05-30 16:38:50 -0500

    minor fixes, works on accepting full streaming data

--- zmq_ex/sub4.py
+++ zmq_ex/sub4.py
@@ -14,7 +14,8 @@ while True:
     files = glob.glob('/home/parallels/stream_transfer/test_files/*')
     #path = '/home/parallels/stream_transfer/test_files/'
     #filename = 'test1.txt'
-    for destfilename in files:
+    for filename in files:
+        destfilename = ntpath.basename(filename)
         #filename = 'personnel.db'
     #destfilename = 'test1.txt'
         destfile = '/home/parallels/stream_transfer/destination/' + destfilename
