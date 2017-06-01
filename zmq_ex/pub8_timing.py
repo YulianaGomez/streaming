cfd1f0d6057ae7dffc98fcd11f6d35acaf81902a
yulianagomez <ygomez297@gmail.com>
2017-06-01 14:42:33 -0500

    Has two handshaking schemes working and gives timing

--- zmq_ex/pub8_timing.py
+++ zmq_ex/pub8_timing.py
@@ -19,6 +19,7 @@ Must figure out the handshake sequence"""
 def transfer():
     #print("in tranfers loop")
     # Prepare context & publisher - must set up context first
+    t0 = time.time()
     context = zmq.Context()
     publisher = context.socket(zmq.PUB)
     publisher.bind("tcp://127.0.0.1:10111")
@@ -31,33 +32,60 @@ def transfer():
     print "Received request: ", msg
     syncservice.send("Message from 10111") #send synchronization reply
 
-    while True:
-        # Read files (names) from fs
-        files = glob.glob('/home/parallels/stream_transfer/test_files/*')
-        if len(files) > 0:
-            print("About the send")
-            for f in files:
-                print 'loop'
-                size = os.stat(f).st_size
-                #print 'File size:',size
+    ##while True: #removed to get none streaming transfer done
+    # Read files (names) from fs
+    files = glob.glob('/home/parallels/stream_transfer/test_files/*')
+    #if len(files) > 0:
+    #    print("About the send")
+    for f in files:
+        print 'loop'
+        size = os.stat(f).st_size
+        #print 'File size:',size
 
-                target = open(f, 'rb')
-                ff = target.read(size)
-                #if file:
-                publisher.send(ff)
-                os.remove(f)
-                #print('Finished removing files from source directory')
-                target.close()
-                #time.sleep(10)
-                #print('Target closed')
+        target = open(f, 'rb')
+        ff = target.read(size)
+        #if file:
+        publisher.send(ff)
+        os.remove(f)
+        #print('Finished removing files from source directory')
+        #target.close()
+        #time.sleep(10)
+        #print('Target closed')
 
     #publisher.close()
     #context.term()
-    print 'Waiting for more data'
+        """Creating second handshake sequence"""
+
+    print 'Finished sending data, waiting for second handshake'
+    syncservice = context.socket(zmq.REP)
+
+    print 'waiting for handshake'
+    syncservice.bind('tcp://127.0.0.1:10113')
+
+    print '...still waiting for handshake'
+    msg = syncservice.recv() ##currently not getting past here
+
+    print "Received request: ", msg
+    syncservice.send("Finished sending")
+    print "past sent part"
+    t1 = time.time()
+    total = t1-t0
+    print ("total time to transfer: %f seconds"%total)
+
+    #target.close()
+    publisher.close()
+    context.term()
+    print"past close and context terminate"
+
+
             ########MAIN##########
 
 if __name__ == '__main__':
+    t0 = time.time()
     transfer()
+    t1 = time.time()
+    total = t1-t0
+    #print ("total time to transfer: %f seconds"%total)
     #while True:
     #    files = glob.glob('/home/parallels/stream_transfer/test_files/*')
 
@@ -74,5 +102,5 @@ if __name__ == '__main__':
     #    time.sleep(1)
     #    print("in else")
 
-    publisher.close()
-    context.term()
+    #publisher.close()
+    #context.term()
--- zmq_ex/sub6_delete.py
+++ zmq_ex/sub6_delete.py
@@ -9,7 +9,12 @@ import ntpath
 """WORKING VERSION - can accept streaming data from pub4.py
 has correct placement of context and terminations, can delete data after
 transfer is complete, waits for more data once transfer is complete
-works with pub5,6,7,8 versions"""
+works with pub5,6,7,8 versions
+Yuliana Zamora
+yzamora@uchicago.edu
+Last worked on: June 1, 2017
+
+"""
 
 context = zmq.Context()
 subscriber = context.socket(zmq.SUB)
@@ -17,39 +22,54 @@ subscriber = context.socket(zmq.SUB)
 subscriber.connect("tcp://127.0.0.1:10111")
 subscriber.setsockopt(zmq.SUBSCRIBE, b'')
 
+
+####CREATING HANDSHAKE sequence############
 #synchronize with publisher
 syncclient = context.socket(zmq.REQ)
 syncclient.connect('tcp://127.0.0.1:10112')
 #msg = subscriber.recv(313344)
 #send a synchronization request
-syncclient.send('sending msg from subscriber')
-
+syncclient.send('This is a message from subscriber')
 #wait for synchronization reply
 syncclient.recv()
+###############################################
 
 print("About to receive")
 
 #def main():
-while True:
+#while True:
 #/home/parallels/stream_transfer/test_files/test1.txt
 
-    files = glob.glob('/home/parallels/stream_transfer/test_files/*')
-    #path = '/home/parallels/stream_transfer/test_files/'
-    #filename = 'test1.txt'
-    for filename in files:
-        destfilename = ntpath.basename(filename)
-        #filename = 'personnel.db'
-    #destfilename = 'test1.txt'
-        destfile = '/home/parallels/stream_transfer/destination/' + destfilename
+files = glob.glob('/home/parallels/stream_transfer/test_files/*')
+#path = '/home/parallels/stream_transfer/test_files/'
+#filename = 'test1.txt'
+for filename in files:
+    destfilename = ntpath.basename(filename)
+    #filename = 'personnel.db'
+#destfilename = 'test1.txt'
+    destfile = '/home/parallels/stream_transfer/destination/' + destfilename
 
-        #msg = subscriber.recv(313344)
-        msg = subscriber.recv()
-        #if msg:
-        f = open(destfile, 'wb')
-        print 'open'
-        f.write(msg)
-        print 'close\n'
-        f.close()
+    #msg = subscriber.recv(313344)
+    msg = subscriber.recv()
+    #if msg:
+    f = open(destfile, 'wb')
+    print 'open'
+    f.write(msg)
+    print 'close\n'
+    f.close()
+
+
+####CREATING HANDSHAKE sequence############
+#synchronize with publisher
+syncclient = context.socket(zmq.REQ)
+syncclient.connect('tcp://127.0.0.1:10113')
+#msg = subscriber.recv(313344)
+#send a synchronization request
+syncclient.send('received all the data')
+#wait for synchronization reply
+syncclient.recv()
+###############################################
+
 ########MAIN##########
 """
 if __name__ == '_main__':
