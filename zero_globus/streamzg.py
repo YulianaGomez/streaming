d2f8e89641ce5663bf65f11bb04852b85f30ec1c
yulianagomez <ygomez297@gmail.com>
2017-06-23 15:10:38 -0500

    Adding server zmq options

--- zero_globus/streamzg.py
+++ zero_globus/streamzg.py
@@ -31,11 +31,11 @@ Date Last Modified:   June 23, 2017
 
 # Input:
 
-def mult_files():
+def mult_files(file_number):
 	filename_default = "filename_test"
 	source_path      = "/home/parallels/stream_transfer/"
 	dest_path        = "/tmp/ramdisk/"
-	max_iterations   = 1000
+	max_iterations   = file_number
 
 	if   len(sys.argv) == 1: filename = filename_default
 	elif len(sys.argv) == 2: filename = sys.argv[1]
@@ -96,17 +96,19 @@ if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
 	parser.add_argument("-z", "--zeromq", dest="zeromq", action = "store_true", default=False)
 	parser.add_argument("-g", "--globus", dest="globus", action = "store_true", default=False)
+	parser.add_argument("-s", "--server", dest="server")
 	parser.add_argument("-m", "--many", dest="many")
 	parser.add_argument("-o", "--one", dest="one")
 	#parser.add_argument("-d" , "--dest_endpoint" , dest="dest_endpoint")
 	#parser.add_argument("-s" , "--source_endpoint" , dest="source_endpoint")
 	args = parser.parse_args()
+	server = args.server
 	globus = args.globus
 	zeromq = args.zeromq
 
 	if zeromq:
 	    print "You are running the client side with ZeroMQ. Waiting for server to send msgs."
-	    client_model.client(0,"","","")
+	    client_model.client(1024,"/home/yzamora/streaming/zero_globus/","140.221.68.131","10120")
 
 	else:
 		if globus:
@@ -115,5 +117,26 @@ if __name__ == "__main__":
 			files = glob.glob('/home/parallels/stream_transfer/test_files/*')
         	if len(files) > 0: 
 				globus_transfer.transfer()
+	else:
+		if server:
+		print "You are starting the server side"
+		file_number = input("How many files would you like to create? Enter 0 for infinite: ")
+		mult_files(file_number)
+		print "Your files are being created and will be sent to client/sub via ZMQ"
+		context = zmq.Context()
+        router = context.socket(zmq.ROUTER)
+        router.bind("tcp://*:10120")
+
+	        while True:
+	        #files = glob.glob('/home/ubuntu/yzamora/stream2/test_files/*')
+	        	files = glob.glob('/home/yzamora/streaming/test_files/*')
+	        	if len(files) > 0:
+	            	t0 = time.time()
+	            	server(router, files)
+	            	break #use if testing for timing
+	        	else:
+	            	time.sleep(1)
+
+
 		else:
 			print "You haven't picked a message passage interface: -z for zeromq or -g for globus"
