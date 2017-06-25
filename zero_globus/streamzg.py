e16f4e6e849d50b6dd3f2dc0f01f6455c7786e18
yulianagomez <ygomez297@gmail.com>
2017-06-24 19:05:43 -0500

    app asking user for files to create, can work with zmq, server side working

--- zero_globus/streamzg.py
+++ zero_globus/streamzg.py
@@ -7,9 +7,11 @@ import subprocess
 import time
 import glob
 import argparse
+import zmq
 
 import globus_transfer
 import client_model
+import server_new
 
 ##============================================================================##
 ##------------------------------- stream_data.py -----------------------------##
@@ -33,8 +35,8 @@ Date Last Modified:   June 23, 2017
 
 def mult_files(file_number):
 	filename_default = "filename_test"
-	source_path      = "/home/parallels/stream_transfer/"
-	dest_path        = "/tmp/ramdisk/"
+	source_path      = "/home/parallels/stream_transfer/zero_globus/test_files/"
+	#dest_path        = "/tmp/ramdisk/"
 	max_iterations   = file_number
 
 	if   len(sys.argv) == 1: filename = filename_default
@@ -49,9 +51,9 @@ def mult_files(file_number):
 	    #fname_src = source_path+filename+"."+str(random_num)
 	    #fname_dst = dest_path+filename+"."+str(random_num)
 	    fname_src = source_path+filename+"."+str(iter)
-	    fname_dst = dest_path+filename+"."+str(iter)
+	    #fname_dst = dest_path+filename+"."+str(iter)
 	    with open(fname_src,"wa") as f: f.write(str(iter)+"\n")
-	    subprocess.call(["mv",fname_src,fname_dst])
+	    #subprocess.call(["mv",fname_src,fname_dst])
 	    iter += 1
 	    if iter == max_iterations:
 	        print "Max Iteration Reached. Done."; break
@@ -96,9 +98,9 @@ if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
 	parser.add_argument("-z", "--zeromq", dest="zeromq", action = "store_true", default=False)
 	parser.add_argument("-g", "--globus", dest="globus", action = "store_true", default=False)
-	parser.add_argument("-s", "--server", dest="server")
-	parser.add_argument("-m", "--many", dest="many")
-	parser.add_argument("-o", "--one", dest="one")
+	parser.add_argument("-s", "--server", dest="server", action = "store_true", default=False)
+	#parser.add_argument("-m", "--many", dest="many")
+	#parser.add_argument("-o", "--one", dest="one")
 	#parser.add_argument("-d" , "--dest_endpoint" , dest="dest_endpoint")
 	#parser.add_argument("-s" , "--source_endpoint" , dest="source_endpoint")
 	args = parser.parse_args()
@@ -110,33 +112,29 @@ if __name__ == "__main__":
 	    print "You are running the client side with ZeroMQ. Waiting for server to send msgs."
 	    client_model.client(1024,"/home/yzamora/streaming/zero_globus/","140.221.68.131","10120")
 
+	elif globus:
+            globus_transfer.service()
+            print "You are running Globus"
+            files = glob.glob('/home/parallels/stream_transfer/test_files/*')
+    	    if len(files) > 0: 
+                globus_transfer.transfer()
+	elif server:
+	    print "You are starting the server side"
+	    file_number = input("How many files would you like to create? Enter 0 for infinite: ")
+	    mult_files(file_number)
+            print "Your files are being created and will be sent to client/sub via ZMQ"
+            context = zmq.Context()
+            router = context.socket(zmq.ROUTER)
+	    router.bind("tcp://*:10120")
+
+            while True:
+                #files = glob.glob('/home/ubuntu/yzamora/stream2/test_files/*')
+        	files = glob.glob('/home/parallels/stream_transfer/zero_globus/test_files/*')
+        	if len(files) > 0:
+            	    t0 = time.time()
+            	    server_new.server(router, files)
+            	    break #use if testing for timing
+        	else:
+            	    time.sleep(1)
 	else:
-		if globus:
-			globus_transfer.service()
-			print "You are running Globus"
-			files = glob.glob('/home/parallels/stream_transfer/test_files/*')
-        	if len(files) > 0: 
-				globus_transfer.transfer()
-	else:
-		if server:
-		print "You are starting the server side"
-		file_number = input("How many files would you like to create? Enter 0 for infinite: ")
-		mult_files(file_number)
-		print "Your files are being created and will be sent to client/sub via ZMQ"
-		context = zmq.Context()
-        router = context.socket(zmq.ROUTER)
-        router.bind("tcp://*:10120")
-
-	        while True:
-	        #files = glob.glob('/home/ubuntu/yzamora/stream2/test_files/*')
-	        	files = glob.glob('/home/yzamora/streaming/test_files/*')
-	        	if len(files) > 0:
-	            	t0 = time.time()
-	            	server(router, files)
-	            	break #use if testing for timing
-	        	else:
-	            	time.sleep(1)
-
-
-		else:
-			print "You haven't picked a message passage interface: -z for zeromq or -g for globus"
+            print "You haven't picked a message passage interface: -z for zeromq or -g for globus"
