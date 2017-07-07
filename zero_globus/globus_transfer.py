def3abe09c33ad7aae5aadc5e5ca86894c95ed8b
yulianagomez <ygomez297@gmail.com>
2017-07-07 16:47:12 -0500

    Updated refresh token part of globus transfers

--- zero_globus/globus_transfer.py
+++ zero_globus/globus_transfer.py
@@ -17,6 +17,54 @@ from os import makedirs
 # ddb59aef-6d04-11e5-ba46-22000b92c6ec
 # 2b5e59d2-409e-11e7-bd30-22000b9a448b
 
+
+#=================================================================================#
+#------------------------------- GLOBUS TOKEN ------------------------------------#
+#=================================================================================#
+CLIENT_ID = '59a91e4c-6717-4301-8d93-f02ff19ffdf0'
+
+client = globus_sdk.NativeAppAuthClient(CLIENT_ID)
+client.oauth2_start_flow(refresh_tokens=True)
+
+authorize_url = client.oauth2_get_authorize_url()
+print('Please go to this URL and login: {0}'.format(authorize_url))
+
+# this is to work on Python2 and Python3 -- you can just use raw_input() or
+# input() for your specific version
+get_input = getattr(__builtins__, 'raw_input', input)
+auth_code = get_input(
+    'Please enter the code you get after login here: ').strip()
+print("Past auth_code input")
+token_response = client.oauth2_exchange_code_for_tokens(auth_code)
+print("received token response")
+globus_auth_data = token_response.by_resource_server['auth.globus.org']
+print("past auth data")
+globus_transfer_data = token_response.by_resource_server['transfer.api.globus.org']
+print("past transfer data")
+# the refresh token and access token, often abbr. as RT and AT
+transfer_rt = globus_transfer_data['refresh_token']
+print("past transfer rt: ", transfer_rt)
+transfer_at = globus_transfer_data['access_token']
+expires_at_s = globus_transfer_data['expires_at_seconds']
+print("past expires")
+# Now we've got the data we need, but what do we do?
+# That "GlobusAuthorizer" from before is about to come to the rescue
+
+authorizer = globus_sdk.RefreshTokenAuthorizer(
+    transfer_rt, client, access_token=transfer_at, expires_at=expires_at_s)
+print("past autorizer")
+# and try using `tc` to make TransferClient calls. Everything should just
+# work -- for days and days, months and months, even years
+tc = globus_sdk.TransferClient(authorizer=authorizer)
+# most specifically, you want these tokens as strings
+#AUTH_TOKEN = globus_auth_data['access_token']
+#TRANSFER_TOKEN = globus_transfer_data['access_token']
+print("past tc: ", tc)
+
+#=================================================================================#
+#------------------------------- GLOBUS TOKEN ------------------------------------#
+#=================================================================================#
+
 #=================================================================================#
 #-------------------------------    transfer    ----------------------------------#
 #=================================================================================#
@@ -25,8 +73,8 @@ def service():
 
 def transfer(files):
     print("Globus transfer will begin")
-    token_authorizer = AccessTokenAuthorizer(access_token = 'AQBZXNu8AAAAAAAFF9V3ePa7fHNGjBlfFpY7FJcrg2JqZa_R-iowMQII6HVsvm5DTKlorRY4Wyk9rN1tzI0K')
-    tc = globus_sdk.TransferClient(token_authorizer)
+    #token_authorizer = AccessTokenAuthorizer(access_token = 'AQBZXNu8AAAAAAAFF9V3ePa7fHNGjBlfFpY7FJcrg2JqZa_R-iowMQII6HVsvm5DTKlorRY4Wyk9rN1tzI0K')
+    #tc = globus_sdk.TransferClient(token_authorizer)
     #UUID can be found at your endpoint at globus or through tokens.globus.org
     #endpoints need to be established before transfer data
     #task list
@@ -129,9 +177,9 @@ def transfer(files):
 #-------------------------------      MAIN      ----------------------------------#
 #=================================================================================#
 
-if __name__ == "__main__":
+#if __name__ == "__main__":
 
-    while True:
+   # while True:
         #my_file = Path("/home/parallels/globus-sdk-python/globusnram/test_files/")
         #if my_file.is_file():
         #    transfer()
