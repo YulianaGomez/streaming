5d47b8da67c78128747dbd66fb606a26cfdcf25f
yulianagomez <ygomez297@gmail.com>
2017-09-12 19:07:48 +0000

    adding ability to call server as well

--- guc.py
+++ guc.py
@@ -36,9 +36,16 @@ def ucopy(source, destination, port):
 
     os.system('globus\-url\-copy \-vb \-p 10' + ' ' + 'ftp://' + ips[source] + ':' + port + '/dev/zero' + ' ' + 'ftp://' + cooley[destination] + ':' + port + '/dev/null'  )
 
+def userver(port):
+    os.system('globus\-gridftp\-server -\debug \-aa \-p' + ' ' + port) 
+
 if __name__ == '__main__':
     #ucopy("yulie1","yulie2",50001)
-    source = sys.argv[1]
-    destination = sys.argv[2]
-    port = sys.argv[3]
-    ucopy(source, destination, port)
+    if len(sys.argv)>2:
+        source = sys.argv[1]
+        destination = sys.argv[2]
+        port = sys.argv[3]
+        ucopy(source, destination, port)
+    else:
+        port = sys.argv[1]
+        userver(port)
