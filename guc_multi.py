82e5d875284c42ada4818183a8ce7b2c36b2e1fb
Chameleon Cloud User <ygomez297@gmail.com>
2017-09-18 02:59:57 +0000

    Running multiple transfers concurrently, added joining of processes to get more accurate timing, added len flag for specific amount of data to be transferred

--- guc_multi.py
+++ guc_multi.py
@@ -1,7 +1,7 @@
 import os
 import sys
-from multiprocessing import Process
-
+import multiprocessing as mp
+import time
 
 ##============================================================================##
 ##----------------------------- globus_urlcopy -------------------------------##
@@ -13,19 +13,23 @@ Purpose:             Calling globus urlcopy with specific endpoints
 Author:               Yuliana Zamora
 Email:                yzamora@uchicago.edu
 Date Created:         August 24, 2017
-Date Last Modified:   August 24, 2017
+Date Last Modified:   September 17, 2017
 '''
 #globus-url-copy -vb -p 10 ftp://129.114.33.196:50003/dev/zero ftp://140.221.68.225:50003/dev/null
 ##============================================================================##
 ##------------------------------globus_urlcopy---- ---------------------------##
 ##============================================================================##
-i
-def f(p, src, port, dest):
-    os.system('globus\-url\-copy \-vb \-p' + ' ' +  p + ' ' + '\-cc 1' + ' ' + 'ftp://' + src + ':' + port + '/dev/zero' + ' ' + 'ftp://' + dest + ':' + port + '/dev/null'  )
 
+n_processes = 4 #Number of globus transfer done in parallel - performance limit is VCPUs available
+tran_size= 10 #limit amount that will be transfered. Thiis is in GB
+
+def myfunc(i, p, src, port, dest):
+    os.system('globus\-url\-copy \-vb \-len ' + str(tran_size) + 'GB \-p' + ' ' + str(p) + ' ' + '\-cc 1' + ' ' + 'ftp://' + src + ':' + port + '/dev/zero' + ' ' + 'ftp://' + dest + ':' + port + '/dev/null'  )
+
+    print "Process: ", i, "is done"
 
 #def ucopy(source,destination,port):
-def ucopy(source, destination, port,p):
+def ucopy(source, destination, port, par):
     ips = {}
     cooley = {}
     with open("cham_ports", 'r') as f:
@@ -38,16 +42,17 @@ def ucopy(source, destination, port,p):
             key = line.split()[0]
             value = line.split()[1]
             cooley[key] = value
-
-    n_processes = 4
+    #n_processes = 4
     myprocs = []
     for i in range(0, n_processes):
-        p = Process(target=f,args=(p,ips[source],port,cooley[destination]))
+        p = mp.Process(target=myfunc,args=(i,par,ips[source],port,cooley[destination]))
         myprocs.append(p)
-        os.system("taskset -c %d -p %d" % ((i % os.cpu_count()), p.pid)) 
         p.start()
+        print " -c -> ",i%mp.cpu_count()," -p ->",p.pid
+        os.system("taskset -p -c "+str(i%mp.cpu_count())+" "+str(p.pid)) 
+        #p.start()
     #os.system('globus\-url\-copy \-vb \-p' + ' ' +  p + ' ' + '\-cc 1' + ' ' + 'ftp://' + ips[source] + ':' + port + '/dev/zero' + ' ' + 'ftp://' + cooley[destination] + ':' + port + '/dev/null'  )
-    #for p in myprocs: p.join()
+    for p in myprocs: p.join()
     
 def userver(port):              
     os.system('globus\-gridftp\-server -\debug \-aa \-p' + ' ' + port) 
@@ -59,7 +64,12 @@ if __name__ == '__main__':
         destination = sys.argv[2]
         port = sys.argv[3]
         p = sys.argv[4]
+        start = time.time()
         ucopy(source, destination, port,p)
+        etime = time.time()
+        tt = etime - start
+        print "Total time : ", tt
+        print "Transfer Rate Calculated: ", (1000.00*tran_size)/tt, "MB/sec"  
     else:
         port = sys.argv[1]
         userver(port)
